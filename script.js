877f72d5fd808205302ebca09f4f8eff0e2d783c
Nayanbarahate <nayanbarahate11@gmail.com>
2022-04-05 19:31:51 +0530

    add files

---
+++ script.js
@@ -0,0 +1,70 @@
+// Create one function with zero parameter having a console statement;
+// Create one function which takes two values as a parameter and print the sum of them as "Sum of 3, 4 is 7"
+// Create one function which takes three parameter as first name and last name and age and return the statement as "A is 99 years old"
+// Create one arrow function
+// "Print output: 
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl ();
+// "Print output: 
+var x = 21;
+girl ();
+console.log(x)
+function girl() {
+    console.log(x);
+    var x = 20;
+};
+// "
+// "Print output
+
+var x = 21;
+a();
+b();
+console.log(a);
+function a () {
+    
+    var x = 20;
+console.log(x);
+};
+
+function b () {
+    
+    var x = 40;
+console.log(x);
+};
+// "Print output
+
+var x = 21;
+a();
+b();
+console.log(a);
+function a () {
+    
+   x = 20;
+  console.log(x);
+};
+function b () {
+    
+    x = 40;
+   console.log(x);
+};
+// "Print output
+
+var x = 21;
+a();
+b();
+console.log(a);
+a = function() {
+    
+   x = 20;
+  console.log(x);
+};
+b = function() {
+    
+    x = 40;
+   console.log(x);
+};
+// Write a function that accepts parameter n and returns factorial of n
